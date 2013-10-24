043eef385ec35de3a631c93106e99e424cafed2e
Wouter de Vos <wouter@springest.com>
2013-10-24 13:15:27 +0200

    Revert "Added all kinds of dependencies."
    This reverts commit ac46c1f33e54e33e5699ffa5af0c325062b70b40.

--- beanstalkd/attributes/default.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook Name:: beanstalkd
-# Attributes:: default
-#
-# Copyright 2012, Escape Studios
-#
-
-default['beanstalkd']['daemon_opts'] = ""
-default['beanstalkd']['start_during_boot'] = false
