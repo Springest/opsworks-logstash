bba3a2a38e971f478f584274a2cf8a443fe55a8d
Wouter de Vos <wouter@springest.com>
2013-10-24 12:03:26 +0200

    RabbitMQ cluster.

---
+++ rabbitmq_cluster/metadata.rb
@@ -0,0 +1,9 @@
+name             "rabbitmq_cluster"
+
+%w{ubuntu debian linuxmint redhat centos scientific amazon fedora oracle smartos suse}.each do |os|
+  supports os
+end
+
+%w{ rabbitmq hosts }.each do |ckbk|
+  depends ckbk
+end
---
+++ rabbitmq_cluster/recipes/default.rb
@@ -0,0 +1,31 @@
+# Add all rabbitmq nodes to the hosts file with their short name.
+include_recipe "hosts"
+
+instances = node[:opsworks][:layers][:rabbitmq][:instances]
+
+instances.each do |name, attrs|
+  hostsfile_entry attrs['private_ip'] do
+    hostname  name
+    unique    true
+  end
+end
+
+rabbit_nodes = instances.map{ |name, attrs| "rabbit@#{name}" }
+node.set['rabbitmq']['cluster_disk_nodes'] = rabbit_nodes
+
+include_recipe 'rabbitmq'
+
+rabbitmq_user "guest" do
+  action :delete
+end
+
+rabbitmq_user node['rabbitmq_cluster']['user'] do
+  password node['rabbitmq_cluster']['password']
+  action :add
+end
+
+rabbitmq_user node['rabbitmq_cluster']['user'] do
+  vhost "/"
+  permissions ".* .* .*"
+  action :set_permissions
+end
